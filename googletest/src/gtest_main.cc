a808a3e86676cefb974e89f65a4fa22b93e6b78c
openvela-robot <openvela-robot@xiaomi.com>
2019-01-08 11:51:16 +0800

    Merge pull request #2051 from enptfb55:master
    PiperOrigin-RevId: 228337465

--- googletest/src/gtest_main.cc
+++ googletest/src/gtest_main.cc
@@ -27,13 +27,17 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <stdio.h>
+#include <cstdio>
 #include "gtest/gtest.h"
 
 #ifdef ARDUINO
 void setup() {
-  int argc = 0;
-  char** argv = nullptr;
+  // Since Arduino doesn't have a command line, fake out the argc/argv arguments
+  int argc = 1;
+  const auto arg0 = "PlatformIO";
+  char* argv0 = const_cast<char*>(arg0);
+  char** argv = &argv0;
+
   testing::InitGoogleTest(&argc, argv);
 }
 
@@ -46,6 +50,4 @@ GTEST_API_ int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
-
 #endif
-
