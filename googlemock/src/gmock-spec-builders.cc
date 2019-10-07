cac5de3ffc5a49706b9196b3bed09f5b8572796e
openvela-robot <openvela-robot@xiaomi.com>
2019-10-07 15:16:08 +0800

    Merge pull request #2495 from hyukmyeong:typo
    PiperOrigin-RevId: 273346440

--- googlemock/src/gmock-spec-builders.cc
+++ googlemock/src/gmock-spec-builders.cc
@@ -34,7 +34,6 @@
 // EXPECT_CALL).
 
 #include "gmock/gmock-spec-builders.h"
-#include "gtest/internal/gtest-port.h"
 
 #include <stdlib.h>
 #include <iostream>  // NOLINT
@@ -71,7 +70,7 @@ GTEST_API_ void LogWithLocation(testing::internal::LogSeverity severity,
                                 const char* file, int line,
                                 const std::string& message) {
   ::std::ostringstream s;
-  s << internal::FormatFileLocation(file, line) << " " << message << ::std::endl;
+  s << file << ":" << line << ": " << message << ::std::endl;
   Log(severity, s.str(), 0);
 }
 
