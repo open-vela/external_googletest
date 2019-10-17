abb8db39695d5633ff5204cd09d7146b9a30885f
liujinye <liujinye@xiaomi.com>
2019-10-17 13:07:51 +0800

    Merge pull request #2507 from roblub:master
    PiperOrigin-RevId: 275076905

--- googletest/src/gtest.cc
+++ googletest/src/gtest.cc
@@ -3220,7 +3220,9 @@ void PrettyUnitTestResultPrinter::OnTestStart(const TestInfo& test_info) {
 void PrettyUnitTestResultPrinter::OnTestPartResult(
     const TestPartResult& result) {
   switch (result.type()) {
-    // If the test part succeeded, we don't need to do anything.
+    // If the test part succeeded, or was skipped,
+    // we don't need to do anything.
+    case TestPartResult::kSkip:
     case TestPartResult::kSuccess:
       return;
     default:
