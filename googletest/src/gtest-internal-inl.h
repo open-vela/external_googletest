a85fba3af788b6c5c802932758171d63ce7be5ed
openvela-robot <openvela-robot@xiaomi.com>
2020-03-20 20:15:24 +0800

    Merge pull request #2746 from Romain-Geissler-1A:master
    PiperOrigin-RevId: 302045808

--- googletest/src/gtest-internal-inl.h
+++ googletest/src/gtest-internal-inl.h
@@ -87,7 +87,6 @@ const char kColorFlag[] = "color";
 const char kFilterFlag[] = "filter";
 const char kListTestsFlag[] = "list_tests";
 const char kOutputFlag[] = "output";
-const char kBriefFlag[] = "brief";
 const char kPrintTimeFlag[] = "print_time";
 const char kPrintUTF8Flag[] = "print_utf8";
 const char kRandomSeedFlag[] = "random_seed";
@@ -169,7 +168,6 @@ class GTestFlagSaver {
     internal_run_death_test_ = GTEST_FLAG(internal_run_death_test);
     list_tests_ = GTEST_FLAG(list_tests);
     output_ = GTEST_FLAG(output);
-    brief_ = GTEST_FLAG(brief);
     print_time_ = GTEST_FLAG(print_time);
     print_utf8_ = GTEST_FLAG(print_utf8);
     random_seed_ = GTEST_FLAG(random_seed);
@@ -192,7 +190,6 @@ class GTestFlagSaver {
     GTEST_FLAG(internal_run_death_test) = internal_run_death_test_;
     GTEST_FLAG(list_tests) = list_tests_;
     GTEST_FLAG(output) = output_;
-    GTEST_FLAG(brief) = brief_;
     GTEST_FLAG(print_time) = print_time_;
     GTEST_FLAG(print_utf8) = print_utf8_;
     GTEST_FLAG(random_seed) = random_seed_;
@@ -215,7 +212,6 @@ class GTestFlagSaver {
   std::string internal_run_death_test_;
   bool list_tests_;
   std::string output_;
-  bool brief_;
   bool print_time_;
   bool print_utf8_;
   int32_t random_seed_;
