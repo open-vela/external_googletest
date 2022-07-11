0d7d07077d98b2d093c98c02bcc2472a8a3709ba
openvela-robot <openvela-robot@xiaomi.com>
2022-07-11 08:46:06 +0800

    Merge pull request #3844 from akohlmey:intel-llvm-utf8-fix
    PiperOrigin-RevId: 460222266
    Change-Id: I2fbeac0b333ca16639f172ca1f23794ea9ddc2ad

--- googletest/include/gtest/internal/gtest-internal.h
+++ googletest/include/gtest/internal/gtest-internal.h
@@ -462,7 +462,7 @@ class TestFactoryBase {
   TestFactoryBase& operator=(const TestFactoryBase&) = delete;
 };
 
-// This class provides implementation of TestFactoryBase interface.
+// This class provides implementation of TeastFactoryBase interface.
 // It is used in TEST and TEST_F macros.
 template <class TestClass>
 class TestFactoryImpl : public TestFactoryBase {
@@ -830,7 +830,8 @@ class TypeParameterizedTestSuite<Fixture, internal::None, Types> {
 // For example, if Foo() calls Bar(), which in turn calls
 // GetCurrentOsStackTraceExceptTop(..., 1), Foo() will be included in
 // the trace but Bar() and GetCurrentOsStackTraceExceptTop() won't.
-GTEST_API_ std::string GetCurrentOsStackTraceExceptTop(int skip_count);
+GTEST_API_ std::string GetCurrentOsStackTraceExceptTop(UnitTest* unit_test,
+                                                       int skip_count);
 
 // Helpers for suppressing warnings on unreachable code or constant
 // condition.
