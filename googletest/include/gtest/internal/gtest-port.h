576fab13ced80dc3073dfe75d83d9d8ce3725abd
openvela-robot <openvela-robot@xiaomi.com>
2022-07-18 10:48:58 +0800

    Merge pull request #3921 from Krzmbrzl:patch-1
    PiperOrigin-RevId: 461660066
    Change-Id: Ie23e046caf24d350c91e4ee3c4baf512780c6984

--- googletest/include/gtest/internal/gtest-port.h
+++ googletest/include/gtest/internal/gtest-port.h
@@ -1710,7 +1710,7 @@ typedef GTestMutexLock MutexLock;
 // C-linkage.  Therefore it cannot be templatized to access
 // ThreadLocal<T>.  Hence the need for class
 // ThreadLocalValueHolderBase.
-class GTEST_API_ ThreadLocalValueHolderBase {
+class ThreadLocalValueHolderBase {
  public:
   virtual ~ThreadLocalValueHolderBase() {}
 };
